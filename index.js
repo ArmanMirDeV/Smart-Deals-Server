df42e2901a91bd5e685f089a77758d17e3b08778
Arman Mir <mirarman8583@gmail.com>
2025-10-31 14:21:34 +0600

    delete one from bids added

--- index.js
+++ index.js
@@ -100,6 +100,12 @@ async function run() {
           res.send(result)
       })
       
+       app.delete("/bids/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) };
+         const result = await bidsCollection.deleteOne(query);
+         res.send(result);
+       });
 
     // Send a ping to confirm a successful connection
 
